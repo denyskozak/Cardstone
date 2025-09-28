8d87604d6c8b24ac8a70b22a2dfcebd840aa45c4
Dionsis.sui <117740720+denyskozak@users.noreply.github.com>
2025-09-28 20:32:26 +0100

    Use board template background

--- client/src/gamepixi/layers/Background.tsx
+++ client/src/gamepixi/layers/Background.tsx
@@ -11,17 +11,7 @@ export default function Background({ width, height }: BackgroundProps) {
 
   return (
     <>
-      <pixiGraphics
-        draw={(g) => {
-          g.clear();
-          g.beginFill(0x2d3436);
-          g.drawRect(0, 0, width, height);
-          g.endFill();
-          g.beginFill(0x3e2723, 0.5);
-          g.drawRoundedRect(32, 32, width - 64, height - 64, 24);
-          g.endFill();
-        }}
-      />
+      <pixiSprite image="/assets/board_template.webp" width={width} height={height} />
       <pixiSprite
         image="/assets/deck_template.webp"
         width={deckWidth}
